b2232ba3a619b9df36d89b81ef36059787cd62df
ivanov-kir-m <ivanov-kir-m@yandex-team.ru>
2023-02-19 18:26:07 +0300

    Task

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/sort/SortUiState.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/sort/SortUiState.java
@@ -1,11 +1,26 @@
 package ru.yandex.practicum.contacts.presentation.sort;
 
 // класс UiState должен иметь package-private доступ
-public class SortUiState {
+class SortUiState {
 
     // сделайте поля isApplyEnable и newSelectedSortType приватными
-    public boolean isApplyEnable = false;
-    public String newSelectedSortType = null;
+    private boolean isApplyEnable = false;
+    private String newSelectedSortType = null;
 
     // реализуйте get и set методы для обоих полей
+    public void setIsApplyEnable(final boolean value){
+        isApplyEnable = value;
+    }
+
+    public boolean getIsApplyEnable(){
+        return isApplyEnable;
+    }
+
+    public void setNewSelectedSortType(final String value){
+        newSelectedSortType = value;
+    }
+
+    public String getNewSelectedSortType(){
+        return newSelectedSortType;
+    }
 }
